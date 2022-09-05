3c2d0cb88011ae605c4ad06fbd15a46978b4e0ac
mhufton <mhufton90@gmail.com>
2022-09-05 11:04:20 -0500

    cleaned up directory

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Hi
+    </div>
+  );
+}
+
+export default App;
