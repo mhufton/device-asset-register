9ddfaa79601f66df285ba6f3999b6a0e49c6b8f8
Mike Hufton <mikehufton@Mikes-MacBook.local>
2022-09-16 09:23:16 +0100

    cors

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
-const { list } = require("./devices/devices.controller");
 
 // knex.migrate.latest()
 //   .then((migrations) => {
